f9c5435b1992436e0e406363f4f746af48db31fc
paha <pahany4@mail.ru>
2023-08-23 20:30:13 +0700

    глава 4. Рефакторинг приложения для упрощения его тестирования

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { engine } from 'express-handlebars';
-import {getFortune} from "./lib/fortune.js";
+import {about, home, notFound, serverError} from "./lib/handlers.js";
 
 const app = express()
 
@@ -14,26 +14,15 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static(process.cwd() + '/public'))
 
-app.get('/', (req, res) => res.render('home'))
+app.get('/', home)
 
-
-
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: getFortune() })
-})
+app.get('/about', about)
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(serverError)
 
 app.listen(port, () => console.log(
   `Express started on http://localhost:${port}; ` +
