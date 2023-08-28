1e783e259f97bf1127f4b34f1229222b6db3521a
paha <pahany4@mail.ru>
2023-08-28 21:43:22 +0700

    Заголовки запроса и ответа.

--- meadowlark.js
+++ meadowlark.js
@@ -4,6 +4,15 @@ import {about, home, notFound, serverError} from "./lib/handlers.js";
 
 const app = express()
 
+/** Отключение заголовка данных о сервере */
+app.disable('x-powered-by')
+
+app.get('*', (req, res) => {
+  res.send(`Open your dev tools and examine your headers; ` +
+    `you'll notice there is no x-powered-by header!`)
+})
+
+
 const port = process.env.PORT || 4000
 
 // configure Handlebars view engine
@@ -17,6 +26,14 @@ app.use(express.static(process.cwd() + '/public'))
 app.get('/', home)
 
 app.get('/about', about)
+
+/** заголовки запроса */
+app.get('/headers', (req, res) => {
+  res.type('text/plain')
+  const headers = Object.entries(req.headers)
+    .map(([key, value]) => `${key}: ${value}`)
+  res.send(headers.join('\n'))
+})
 
 // custom 404 page
 app.use(notFound)
