0a7fb9efd5c963759bd6c588a03364d85a4804e3
paha <pahany4@mail.ru>
2023-09-12 22:43:04 +0700

    Примеры промежуточного ПО.

---
+++ 01-routing-example.js
@@ -0,0 +1,57 @@
+import express from "express";
+const app = express()
+
+app.use((req, res, next) => { 
+	console.log('\n\nALLWAYS')
+
+	/** Второй запрос браузер сделал за favicon.ico
+		console.log('\n\nALLWAYS', req.method, req.url)
+	*/
+	next()
+})
+
+app.get('/a', (req, res) => { 
+	console.log('/a: route terminated')
+	res.send('a')
+})
+app.get('/a', (req, res) => { 
+	console.log('/a: never called');
+})
+app.get('/b', (req, res, next) => { 
+	console.log('/b: route not terminated')
+	next()
+})
+app.use((req, res, next) => {
+	console.log('SOMETIMES')
+	next()
+})
+app.get('/b', (req, res, next) => {
+	console.log('/b (part 2): error thrown' )
+	throw new Error('b failed')
+})
+app.use('/b', (err, req, res, next) => {
+	console.log('/b error detected and passed on')
+	next(err)
+})
+app.get('/c', (err, req) => {
+	console.log('/c: error thrown')
+	throw new Error('c failed')
+})
+app.use('/c', (err, req, res, next) => {
+	console.log('/c: error detected but not passed on')
+	next()
+})
+
+app.use((err, req, res, next) => {
+	console.log('unhandled error detected: ' + err.message)
+	res.send('500 - server error')
+})
+
+app.use((req, res) => {
+	console.log('route not handled')
+	res.send('404 - not found')
+})
+
+const port = process.env.PORT || 4001
+app.listen(port, () => console.log( `Express started on http://localhost:${port}` +
+  '; press Ctrl-C to terminate.'))
